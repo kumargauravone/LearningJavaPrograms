3f4422868241bc7bc49c5ffa9c5226255afd6846
KumarGarurav <KumarGarurav@192.168.1.198>
2025-05-13 18:16:28 +0530

    Added README.md file.

---
+++ src/main/java/com/LearningJavaPrograms/App.java
@@ -0,0 +1,11 @@
+package com.LearningJavaPrograms;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Will provide the all the core jave program ");
+    }
+}
